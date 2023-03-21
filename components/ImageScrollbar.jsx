11c3bc4469d09b37ecd3783a1f130024819b26e8
Prangon-00 <mahbubasultanaprangon@gmail.com>
2023-03-21 14:46:47 +0600

    final commit

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { Box, Icon, Flex } from '@chakra-ui/react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
-//import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 
 const LeftArrow = () => {
     const { scrollPrev } = useContext(VisibilityContext);
@@ -48,10 +48,10 @@ const ImageScrollbar = ({ data }) => (
              src={item.url} 
              width={1000} 
              height={500}
-             sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px "
+             sizes="(max-width:500px) 100px, (max-width):1023px 400px, 1000px"
              />
         </Box>
       ))}
     </ScrollMenu>
 )
-export default ImageScrollbar;
+export default dynamic (() => Promise.resolve(ImageScrollbar), {ssr: false})
